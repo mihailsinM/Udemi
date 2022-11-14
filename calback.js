6d93286b43e941b144ce511232dfe999337af3a9
Michael <mihailsin49@gmail.com>
2022-11-15 00:07:29 +0200

    add callback.js

---
+++ calback.js
@@ -0,0 +1,30 @@
+function first() {
+
+    //do something
+
+    setTimeout(function () {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`I learn: ${lang}`);
+    callback();
+}
+
+// learnJS("JavaScript", function(){
+//     console.log("I learned this lesson!");
+// });
+
+function done() {
+    console.log("I learned this lesson!");
+}
+
+learnJS("JavaScript", done);
