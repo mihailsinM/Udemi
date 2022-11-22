13427bee65bf146377991f0fa41506efa814819a
Michael <mihailsin49@gmail.com>
2022-11-22 11:23:02 +0200

    add practice_objects_lists.js

---
+++ practice_objects_lists.js
@@ -0,0 +1,65 @@
+
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
+        },
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
+        }
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
+};
+
+function isBudgetEnough(data) {
+    let square = 0;
+    let volume = 0;
+
+    data.shops.forEach(shop => {
+        square += shop.width * shop.length;
+    });
+
+    volume = data.height * square;
+
+    if (data.budget - (volume * data.moneyPer1m3) >= 0) {
+        return "Enough budget.";
+    } else {
+        return "Budget is not enough.";
+    }
+}
+console.log(isBudgetEnough(shoppingMallData));
+
+//--------------------------------------------------------------------------------------------
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i <= arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+    return [a, b, c, `Remaining students: ${rest.length === 0 ? '-' : rest.join(', ')}`];
+}
+console.log(sortStudentsByGroups(students));
