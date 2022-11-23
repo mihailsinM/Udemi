79a6c3d409aef1b3c3506a0320a005ace5741f9c
Michael <mihailsin49@gmail.com>
2022-11-23 14:01:52 +0200

    add test.js

--- test.js
+++ test.js
@@ -1,14 +1,53 @@
-// let i = 1;
+// -1) Какое будет выведено значение: let x = 5; alert( x++ ); ?
 
-// for (i = 0; i >= 3; i++) {
-//     console.log(i);
+// let x = 5;
+// alert(x++);
+// alert(++x);
 
-//     for (i = 0; i >= 4; i++) {
-//         console.log(i);
-//     }
+// -2) Чему равно такое выражение: [] + false - null + true ?
 
-// }
+// console.log([] + false - null + true);  //NaN
 
-for (let i = 0; i >= 10; i++) {
-    console.log("" + i);
-}
+// -3) Что выведет этот код: let y = 1; let x = y = 2; alert(x); ?
+
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+
+// -4) Чему равна сумма [ ] + 1 + 2?
+
+// console.log([] + 1 + 2); // concatenation
+
+// -5) Что выведет этот код: alert( &quot;1&quot;[0] )?
+
+// alert('1'[0]);
+
+// -6) Чему равно 2 &amp;&amp; 1 &amp;&amp; null &amp;&amp; 0 &amp;&amp; undefined ?
+
+// console.log(2 && 1 && null && 0 && undefined);
+
+// -6) Есть ли разница между выражениями? !!( a &amp;&amp; b ) и (a &amp;&amp; b)?
+
+// console.log(!!(1 && 2) === (1 && 2));
+
+// -7) Что выведет этот код: alert( null || 2 &amp;&amp; 3 || 4 ); ?
+
+// alert( null || 2 && 3 || 4 );
+
+// - 8) a = [1, 2, 3]; b = [1, 2, 3]; Правда ли что a == b ?
+// not
+
+// a = [1, 2, 3]; b = [1, 2, 3];
+// console.log(a == b );
+
+// -9) Что выведет этот код: alert( +&quot;Infinity&quot; ); ?
+
+// alert( + "Infinity&quot" );
+
+// -10) Верно ли сравнение: &quot;Ёжик&quot; &gt; &quot;яблоко&quot;?
+
+// console.log("Ёжик" > "яблоко");
+
+// -11) Чему равно 0 || &quot;&quot; || 2 || undefined || true || falsе ?
+
+// console.log(0 || "" || 2 || undefined || true || falsе);
