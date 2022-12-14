093637991152a045502421f1890030dabf914640
Michael <mihailsin49@gmail.com>
2022-12-14 20:11:22 +0200

    add optional_operator

---
+++ optional_operator/script.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const box = document.querySelector('.box');
+const block = document.querySelector('.block');
+
+console.log(block);
+
+// console.log(block?.textContent);
+
+// block?.textContent = '1322';
+
+// if(block){
+//     console.log(block.textContent);
+// }
+
+console.log(1 + 2);
+
+const userData = {
+    name: 'Ivan',
+    age: null,
+    say: function () {
+        console.log('Hello!');
+    }
+}
+
+userData.say();
+userData.hey?.();
+
+// if(userData && userData.skills && userData.skills.js){
+//     console.log(userData.skills.js);
+// }
+
+// console.log(userData.skills.js);
+
+console.log(userData?.skills?.js);
