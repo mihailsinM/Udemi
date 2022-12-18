419fb2b0145944bb888fd4569b77f50f93bbd7de
Michael <mihailsin49@gmail.com>
2022-12-18 12:31:49 +0200

    add bigint

---
+++ bigint/script.js
@@ -0,0 +1,9 @@
+// const bigint = 123315746545262626221857421518654154751654n;
+const sameBigint = BigInt(123315746545262626221857421518654154751654);
+
+// console.log(typeof (bigint));
+
+let bigint = 1n;
+let number = 2;
+
+console.log(bigint + BigInt(number));
