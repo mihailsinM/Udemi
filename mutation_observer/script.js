99402db3109a5ac5e1289ee87047851c9d9f3e11
Michael <mihailsin49@gmail.com>
2023-01-09 13:17:20 +0200

    add mutation_observer

---
+++ mutation_observer/script.js
@@ -0,0 +1,11 @@
+const box = document.querySelector('.box');
+
+let observer = new MutationObserver(mutationRecords => {
+    console.log(mutationRecords);
+});
+
+observer.observe(box, {
+    childList: true
+});
+
+observer.disconnect();
