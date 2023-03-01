0b1eee465715c0033ad400b7dc48c8acc7924cbb
Michael <mihailsin49@gmail.com>
2023-03-01 15:06:11 +0200

    fixed animation

--- setTimeout/script.js
+++ setTimeout/script.js
@@ -1,27 +1,42 @@
-const btn = document.querySelector('.btn');
+const btn = document.querySelector('.btn'),
+    elem = document.querySelector('.box');
+let pos = 0;
 
-let timerId,
-    i = 0;
+
+// let timerId,
+//     i = 0;
+
+// function myAnimation() {
+//     const elem = document.querySelector('.box');
+//     let pos = 0;
+
+//     const id = setInterval(frame, 10);
+
+//     function frame() {
+//         if (pos == 300) {
+//             clearInterval(id);
+//         } else {
+//             pos++;
+//             elem.style.top = pos + 'px';
+//             elem.style.left = pos + 'px';
+//         }
+//     }
+// }
 
 function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
+    pos++;
+    elem.style.top = pos + 'px';
+    elem.style.left = pos + 'px';
 
-    const id = setInterval(frame, 10);
-
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + 'px';
-            elem.style.left = pos + 'px';
-        }
+    if (pos < 300) {
+        requestAnimationFrame(myAnimation);
     }
 }
 
-btn.addEventListener('click', myAnimation);
+btn.addEventListener('click', () => requestAnimationFrame(myAnimation));
 
+let id = requestAnimationFrame(myAnimation);
+cancelAnimationFrame(id);
 
 // const timerId = setTimeout(function (text) {
 //     console.log(text);
